18693bc95e0c2c736d4f60a0530824cd40b42662
Dino <kill_death@163.com>
2018-04-15 14:31:53 +0800

    Demo初步搭建

---
+++ ProxyPatternDemo/ProxyPatternDemo/Controller/DORootVC.m
@@ -0,0 +1,24 @@
+//
+//  DORootVC.m
+//  ProxyPatternDemo
+//
+//  Created by 魏欣宇 on 2018/4/15.
+//  Copyright © 2018年 Dino. All rights reserved.
+//
+
+#import "DORootVC.h"
+
+@interface DORootVC ()
+
+@end
+
+@implementation DORootVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"代理模式";
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+@end
